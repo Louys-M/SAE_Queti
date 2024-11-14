0b732fe6b373a5888c55dae8f3d1138ad600d599
haybeeden <162558115+haybeeden@users.noreply.github.com>
2024-11-14 10:47:28 +0100

    function insects

--- queti-back/app/Http/Controllers/InsectController.php
+++ queti-back/app/Http/Controllers/InsectController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Insect;
 use Illuminate\Http\Request;
 
 class InsectController extends Controller
 {
-    // 
+    public function index (Request $request){
+        $insects = Insect::where('nom_sc', '!=', '');
+
+        $insects = $insects->get();
+        // recherche tous les Product de la table products
+        return response()->json([
+            'status' => true,
+            'message' => 'Product List Successfully',
+            'data' => $insects
+        ], 200); // code 200, tout va bien
+    }
+
+    public function famille (Request $request){
+        $insects = Insect::where('famille_id', '==', $request);
+
+        $insects = $insects->get();
+        // recherche tous les Product de la table products
+        return response()->json([
+            'status' => true,
+            'message' => 'Product List Successfully',
+            'data' => $insects
+        ], 200); // code 200, tout va bien
+    }
 }
